031a98a5459e97de26a51eac5b233e4ce132b3e7
cjyiz <cjyiz@aliyun.com>
2019-11-26 17:18:57 +0800

    局部修复sidebar,使得层级菜单可以架子啊

--- src/router/index.js
+++ src/router/index.js
@@ -1,45 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Layout from '@/layout/index.vue'
+import Layout from '@/layout/index.vue'
 
 Vue.use(Router)
 export const constantRoutes = [
-  {
-    path: '/',
-    name: 'dashboard',
-    component: () => import('@/layout/index.vue'),
-    redirect: '/dasboard',
-    children: [
-      {
-        path: 'dashboard',
-        name: 'Dashboard',
-        component: () => import('@/views/Dashboard.vue'),
-        meta: { title: 'Dashboard', icon: 'dashboard' }
-      }
-    ]
-  },
-  {
-    path: '/dasboard',
-    name: 'dashboard',
-    component: () => import('@/layout/index.vue'),
-    children: [
-      {
-        path: 'dashboard',
-        name: 'Dashboard',
-        component: () => import('@/views/Dashboard.vue'),
-        meta: { title: 'Dashboard', icon: 'dashboard' }
-      }
-    ]
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-
-  },
-  {
-    path: 'table',
-    name: 'table',
-    component: () => import('@/views/Table.vue')
-  },
   {
     path: '/login',
     name: 'login',
@@ -49,7 +13,53 @@ export const constantRoutes = [
     path: '/404',
     name: '404',
     component: () => import('@/views/404.vue')
+  },
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        name: 'Dashboard',
+        component: () => import('@/views/Dashboard.vue'),
+        meta: { title: 'Dashboard', icon: 'dashboard' }
+      }
+    ]
+  },
+  {
+    path: '/example',
+    name: 'Example',
+    component: Layout,
+    redirect: '/example/table',
+    meta: { title: 'Example', icon: 'example' },
+    children: [
+      {
+        path: 'table',
+        name: 'Table',
+        component: () => import('@/views/Table.vue'),
+        meta: { title: 'Table', icon: 'table' }
+      }, {
+        path: 'tree',
+        name: 'Tree',
+        component: () => import('@/views/Tree.vue'),
+        meta: { title: 'Tree', icon: 'tree' }
+      }
+    ]
+  },
+  {
+    path: '/form',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'Form',
+        component: () => import('@/views/Form.vue'),
+        meta: { title: 'Form', icon: 'form' }
+      }
+    ]
   }
+
 ]
 const createRouter = () => new Router({
   // mode: 'history', // require service support
